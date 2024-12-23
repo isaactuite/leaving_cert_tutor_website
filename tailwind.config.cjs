2aebdfc21a1c6ada0cee5adaaadec79d1fc992e0
isaactuite <isaac.tuite@gmail.com>
2024-12-23 14:50:12 +0000

    idk

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./*.html", // Include all HTML files in the root directory
+      "./**/*.html", // Include HTML files in subdirectories
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
